195b94c8fd45047d238add24269270685afc1ed6
iudchenko <andrii.iudchenko@gmail.com>
2023-08-31 12:46:58 +0200

    vite env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://dlvrpzopevdnqqhffetg.supabase.co",
-  process.env.REACT_APP_SUPABASE_API_KEY
+  import.meta.env.VITE_REACT_APP_SUPABASE_URL,
+  import.meta.env.VITE_REACT_APP_SUPABASE_API_KEY
 );
