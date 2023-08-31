0f0a56728389bf0849ee98d5fdb0a38ee388dd60
iudchenko <andrii.iudchenko@gmail.com>
2023-08-31 11:56:54 +0200

    env variables added on netlify

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://dlvrpzopevdnqqhffetg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsdnJwem9wZXZkbnFxaGZmZXRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzODY2MjcsImV4cCI6MjAwODk2MjYyN30.bOnLTDMz5fybrEsLMUm12gWC7Nkdx0q2F6gFLwJm_wA"
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_API_KEY
 );
